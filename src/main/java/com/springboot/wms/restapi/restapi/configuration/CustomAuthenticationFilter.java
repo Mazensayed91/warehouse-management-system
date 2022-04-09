22fefe6991a97f83ac8f0ae66dabbf7250696c9f
mazenbayoumi <sayedmazen70@gmail.com>
2022-04-09 16:32:32 +0200

    1- added roles list for each user, 2- return tokens in json format

--- src/main/java/com/springboot/wms/restapi/restapi/configuration/CustomAuthenticationFilter.java
+++ src/main/java/com/springboot/wms/restapi/restapi/configuration/CustomAuthenticationFilter.java
@@ -2,6 +2,7 @@ package com.springboot.wms.restapi.restapi.configuration;
 
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.springboot.wms.restapi.restapi.User.UserRepository;
 import com.springboot.wms.restapi.restapi.User.UserServiceImpl;
 import lombok.extern.slf4j.Slf4j;
@@ -21,7 +22,11 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.stream.Collectors;
+
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
 @Slf4j
 public class CustomAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
@@ -63,8 +68,15 @@ public class CustomAuthenticationFilter extends UsernamePasswordAuthenticationFi
                 .withIssuer(request.getRequestURL().toString())
                 .sign(algorithm);
 
-        response.setHeader("access_token", accessToken);
-        response.setHeader("refresh_token", refreshToken);
+
+        Map<String, String> tokens = new HashMap<>();
+
+        tokens.put("access_token", accessToken);
+        tokens.put("refresh_token", refreshToken);
+
+        response.setContentType(APPLICATION_JSON_VALUE);
+
+        new ObjectMapper().writeValue(response.getOutputStream(), tokens);
     }
 
 }
