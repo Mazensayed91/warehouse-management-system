9184850d78396a27d1fa5e222f80ee667528876e
mazenbayoumi <sayedmazen70@gmail.com>
2022-04-10 00:39:05 +0200

    secured some endpoints to be accessed via some roles

---
+++ src/main/java/com/springboot/wms/restapi/restapi/configuration/CustomAuthorizationFilter.java
@@ -0,0 +1,62 @@
+package com.springboot.wms.restapi.restapi.configuration;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTVerifier;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.interfaces.DecodedJWT;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collection;
+
+import static java.util.Arrays.stream;
+import static org.springframework.http.HttpHeaders.AUTHORIZATION;
+
+@Slf4j
+public class CustomAuthorizationFilter extends OncePerRequestFilter {
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        // first don't apply the filter if the user is searching for /login
+        if(request.getServletPath().equals("/api/login")){
+            filterChain.doFilter(request, response);
+        } else{
+            String authorizationHeader = request.getHeader(AUTHORIZATION);
+            if(authorizationHeader != null && authorizationHeader.startsWith("Bearer ")){
+                try {
+                    // Verify the token and grant the user his permissions
+                    String token = authorizationHeader.substring("Bearer ".length());
+                    Algorithm algorithm = Algorithm.HMAC256("secret".getBytes());
+                    JWTVerifier verifier = JWT.require(algorithm).build();
+                    DecodedJWT decodedJWT = verifier.verify(token);
+                    String email = decodedJWT.getSubject();
+                    String[] roles = decodedJWT.getClaim("roles").asArray(String.class);
+                    Collection<SimpleGrantedAuthority> authorities = new ArrayList<>();
+                    stream(roles).forEach(role -> {
+                        authorities.add(new SimpleGrantedAuthority(role));
+                    });
+                    UsernamePasswordAuthenticationToken authenticationToken =
+                            new UsernamePasswordAuthenticationToken(email, null, authorities);
+
+                    SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+                    filterChain.doFilter(request, response);
+
+                }catch (Exception e){
+                    log.error("Error logging in: {} ", e.getMessage());
+                    response.setHeader("error", e.getMessage());
+                    response.sendError(HttpServletResponse.SC_FORBIDDEN);
+                }
+            } else{
+                filterChain.doFilter(request, response);
+            }
+        }
+    }
+}
--- src/main/java/com/springboot/wms/restapi/restapi/configuration/WebSecurityConfiguration.java
+++ src/main/java/com/springboot/wms/restapi/restapi/configuration/WebSecurityConfiguration.java
@@ -1,10 +1,13 @@
 package com.springboot.wms.restapi.restapi.configuration;
 
+import com.springboot.wms.restapi.restapi.Permission.PermissionRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
@@ -17,6 +20,9 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+import static javax.swing.text.html.FormSubmitEvent.MethodType.GET;
+
+@Slf4j
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
@@ -24,6 +30,7 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     private final UserDetailsService userDetailsService;
     private final BCryptPasswordEncoder bCryptPasswordEncoder;
+    @Autowired transient PermissionRepository permissionRepository;
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception{
@@ -32,11 +39,19 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception{
+        log.info(permissionRepository.findAll().toString());
+        log.info("Heree");
         http.csrf().disable();
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-        http.authorizeRequests().anyRequest().permitAll();
+        http.authorizeRequests().antMatchers(HttpMethod.POST, "/login").permitAll();
+        http.authorizeRequests().antMatchers(HttpMethod.POST, "/api/permissions/**").permitAll();
+        http.authorizeRequests().antMatchers(HttpMethod.POST, "/api/role/**").permitAll();
+        http.authorizeRequests().antMatchers(HttpMethod.POST, "/api/users/**").permitAll();
+        http.authorizeRequests().antMatchers(HttpMethod.GET, "/api/users/**").permitAll();
+        http.authorizeRequests().antMatchers(HttpMethod.POST, "/api/advices/**").hasAnyAuthority("ROLE_SUPPLIER");
+        http.authorizeRequests().anyRequest().authenticated();
         http.addFilter(new CustomAuthenticationFilter(authenticationManagerBean()));
-
+        http.addFilterBefore(new CustomAuthorizationFilter(), UsernamePasswordAuthenticationFilter.class);
     }
 
     @Bean
